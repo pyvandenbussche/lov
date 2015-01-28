01c81b0f3382ec141cc31a65691ad0a60c184ec1
Pierre-Yves Vandenbussche <py.vandenbussche@gmail.com>
2015-01-28 13:49:25 +0000

    Handle cross domain CORS

--- config/express.js
+++ config/express.js
@@ -70,6 +70,14 @@ module.exports = function (app, config, passport) {
         collection : 'sessions'
       })
     }))
+    
+    //allow cross domain CORS
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Methods', 'GET');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    })
 
     // use passport session
     app.use(passport.initialize())
