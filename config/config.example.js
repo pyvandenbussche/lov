5cc5b23d631ffeaea94299beefc09cb0799db94d
Pierre-Yves Vandenbussche <py.vandenbussche@gmail.com>
2015-08-27 14:55:53 +0100

    Add missing config.example.js file

---
+++ config/config.example.js
@@ -0,0 +1,42 @@
+
+var path = require('path')
+  , rootPath = path.normalize(__dirname + '/..')
+  , templatePath = path.normalize(__dirname + '/../app/mailer/templates')
+  , notifier = {
+      service: 'postmark',
+      APN: false,
+      email: false, // true
+      actions: ['comment'],
+      tplPath: templatePath,
+      key: 'POSTMARK_KEY',
+      parseAppId: 'PARSE_APP_ID',
+      parseApiKey: 'PARSE_MASTER_KEY'
+    }
+
+module.exports = {
+  development: {
+    db: 'mongodb://localhost/lov',
+    es: {host: 'localhost',port: 9200},
+    elasticsearch: {host: 'localhost:9200'},
+    email: {
+      service: 'Gmail',
+      auth: {
+          user: 'user@gmail.com',
+          pass: 'pwd'
+      }
+    }
+  },
+  test: {
+    db: 'mongodb://localhost/lov',
+    es: {host: 'localhost',port: 9200},
+    elasticsearch: {host: 'localhost:9200'},
+    email: {
+      service: 'Gmail',
+      auth: {
+          user: 'user@gmail.com',
+          pass: 'pwd'
+      }
+    }
+  },
+  production: {}
+}
